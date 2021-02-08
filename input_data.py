508c48d18a33b54d60801733c65512d8d131c098
Kenny O <kenny_oliver@icloud.com>
2021-02-08 22:01:22 +0000

    Putting procedures in files

--- input_data.py
+++ input_data.py
@@ -4,7 +4,7 @@ def input_data():
   database_file = open("database_customers.csv", 'a')
   reader = csv.reader(database_file)
   
-  inputRecord = str(input("Enter record in this format:\nForename, Surname, Gender, Age, Current Country, Vehicle, Deposit\n• Commas must be used! •\n--> "))
+  inputRecord = str(input("Enter record in this format:\nForename, Surname, Gender, Age, Current Country, Vehicle, Deposit\n• Commas must be used! •\n• Spaces too! •\n--> "))
   inputRecord.split(", ")
   '''forename, surname, gender, age, currentCountry, vehicle, deposit = inputRecord[0], inputRecord[1], inputRecord[2], inputRecord[3], inputRecord[4], inputRecord[5], inputRecord[6]'''
   forename =        inputRecord[0] 
---
+++ new_section.py
@@ -0,0 +1,5 @@
+import colorama
+from colorama import Fore,Style
+
+def new_section (): #pattern to make UI split into sections
+  print(Fore.WHITE,Style.BRIGHT,">--------------------<\n",Style.RESET_ALL)
