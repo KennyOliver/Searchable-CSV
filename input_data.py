c19ea993fc857d722a099aadfa8031c2720932ae
Kenny O <kenny_oliver@icloud.com>
2021-02-09 07:41:12 +0000

    [+] Use Bash to install Colorama each time, even though refresh deletes it from cache
    [+] Fixed Bash script so that commands run in sequence, rather than parallel
    
    [+] Removed unnecessary code from input_data.py

--- input_data.py
+++ input_data.py
@@ -6,7 +6,6 @@ def input_data():
   
   inputRecord = str(input("Enter record in this format:\nForename, Surname, Gender, Age, Current Country, Vehicle, Deposit\n• Commas must be used! •\n• Spaces too! •\n--> "))
   inputRecord.split(", ")
-  '''forename, surname, gender, age, currentCountry, vehicle, deposit = inputRecord[0], inputRecord[1], inputRecord[2], inputRecord[3], inputRecord[4], inputRecord[5], inputRecord[6]'''
   forename =        inputRecord[0] 
   surname =         inputRecord[1] 
   gender =          inputRecord[2] 
@@ -15,5 +14,5 @@ def input_data():
   vehicle =         inputRecord[5] 
   deposit =         inputRecord[6] 
   #database_file.write(forename, surname, gender, age, currentCountry, vehicle, deposit)
-  database_file.write("\n" + inputRecord)
+  database_file.write(inputRecord + "\n")
   database_file.close()
